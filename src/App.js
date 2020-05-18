7671687b66806890649b2f4b00492a195085a89a
hi2yeoni <hi2yeoni@naver.com>
2020-05-18 20:36:19 +0900

    api using hook

--- src/App.js
+++ src/App.js
@@ -2,43 +2,20 @@
 import React from 'react';
 //import axios from 'axios';
 import Weather from './Weather';
+import GetApi from "./GetApi"
 
 class App extends React.Component {
   state = {
-    isLoading: true,
-    weather: null
+    isLoading: true
   };
-  getWeather = ({ latitude, longitude }) => { //GPS 받은 주소로 api 연결
-    const API_KEY = '894c0c1d03546d1843b5efd334d6e479';
-    fetch(`https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-      .then(response => response.json()) //json으로 변환
-      .then(data => {
-        console.log("Test!", data);
-        this.setState({ isLoading: false, weather: data })
-      });
-  }
   componentDidMount() { //GPS
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.getWeather(position.coords);
-    },
-      (error) => {
-        console.log(error);
-      }
-    );
+    document.title = "My Diary"
   }
 
   render() {
-    const { isLoading, weather } = this.state;
     return (
       <div>
-        {isLoading ? <h1>Weather</h1> :
-          <Weather
-            location={weather.name}
-            weather={weather.weather[0].main}
-            temp={weather.main.temp}
-            key = {weather.id}
-          />
-        }
+        <GetApi/>
       </div>
     );
   }
---
+++ src/GetApi.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react"
+import Weather from "./Weather"
+
+const GetApi = () => {
+    //const [longitude, setLongitude] = useState();
+    const [weather, setWeather] = useState();
+    const API_KEY = '894c0c1d03546d1843b5efd334d6e479';
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            getWeather(position.coords);
+        },
+            (error) => {
+                console.log(error);
+            }
+        )
+    }, []);
+    const getWeather = ({ latitude, longitude }) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?&lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+            .then(response => response.json()) //json으로 변환
+            .then(data => setWeather(data))
+
+        console.log("Test", weather);
+    }
+    const isLoading = (weather == null);
+    return (
+        <div>
+            {isLoading ? <h1>Weather</h1> :
+                <Weather
+                    location={weather.name}
+                    weather={weather.weather[0].main}
+                    temp={weather.main.temp}
+                    key={weather.id}
+                />
+            }
+        </div>
+    );
+}
+
+export default GetApi;
